28ba1eef9cb3c0d8c7c12bd089a3f3b705dcc586
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-10-13 18:31:10 -0500

    Created Song Model for librarybackend app + Made Migrations to MySQL Workbench

--- librarybackend/models.py
+++ librarybackend/models.py
@@ -1,3 +1,12 @@
+from time import time
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField('Release Date', default=timezone.now)
+    genre = models.CharField(max_length=255)
